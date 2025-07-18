4d7d7c21f751a39bddaa4f106e8577a44ef06ab8
Vitor Rodrigues Peclat <114704625+vitorpeclat@users.noreply.github.com>
2025-07-18 14:15:31 -0300

    Tudo

---
+++ APIminima/TodoDb.cs
@@ -0,0 +1,10 @@
+﻿namespace APIminima
+{
+    using Microsoft.EntityFrameworkCore;
+
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
